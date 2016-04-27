1d3728aa933e13888dafcac583321952aab94ba7
Jordan Cowart <j.cowart@Yogi.local>
2016-04-27 02:55:10 -0500

    updated main to use fancy new operator suffixes for strings and generally cleaned up code. Added .gitignore to project and gitignore to templates

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 // put template directory here
 #define TEMPLATE_DIR "/opt/templates/c++/"
 
+using namespace std::literals;
+
 int main(int argc, char ** argv) {
     
     //verify proper number of arguments
@@ -18,35 +20,37 @@ int main(int argc, char ** argv) {
     //arg 1 should be name of project
     auto proj_name = argv[1];
     
+    //define some strings that will be used repeatidly
+    auto cp = "cp "s;
+    auto mkdir = "mkdir "s;
+    auto vscode = "/.vscode"s;
+    auto template_dir = std::string(TEMPLATE_DIR);
+    
     //create project directory
-    auto proj_dir = std::string("mkdir ") + proj_name;
+    auto proj_dir = mkdir + proj_name;
     system(proj_dir.c_str());
     
     //create .vscode directory
-    auto vs_dir = std::string("mkdir ") + proj_name + std::string("/.vscode");
+    auto vs_dir = mkdir + proj_name + vscode;
     system(vs_dir.c_str());
     
     //copy template files
-    auto tasks = std::string("cp ") + std::string(TEMPLATE_DIR) + std::string("tasks.json ") +
-                 proj_name + std::string("/.vscode");
-                 
-                 
+    auto tasks = cp + template_dir + "tasks.json "s + proj_name + vscode;             
     system(tasks.c_str());
     
-    auto cmake = std::string("cp ") + std::string(TEMPLATE_DIR) + std::string("CMakeLists.txt ") +
-                 proj_name + std::string("/");
-                 
+    auto cmake = cp + template_dir + "CMakeLists.txt "s + proj_name + "/"s;            
     system(cmake.c_str());
     
-    // create main file
-    auto create_main = std::string("echo '' > ") + proj_name + std::string("/main.cpp");
+    auto git_ignore = cp + template_dir + "gitignore "s + proj_name + "/.gitignore"s;
+    system(git_ignore.c_str());
     
+    // create main file
+    auto create_main = "echo '' > "s + proj_name + "/main.cpp"s;
     system(create_main.c_str());
     
-    auto git = std::string("git init ") + proj_name + std::string("/");
-    
+    //initialize git repo
+    auto git = "git init "s + proj_name + "/"s;
     system(git.c_str());
     
-    return 0; 
-    
+    return 0;  
 }
